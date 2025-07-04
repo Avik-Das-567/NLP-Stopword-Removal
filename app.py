8031c786a7f726b00fcaccc00c656be8509e896f
Avik Das <avikdas567@gmail.com>
2025-07-04 14:31:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ words = word_tokenize(text)
 filtered_words = [w for w in words if w.lower() not in stopwords.words('english')]
 
 st.write("Input Text :-", text)
-st.write("After removing Stopwords, Output is :", filtered_words)
+st.write("After removing Stopwords, Output is :-", filtered_words)
